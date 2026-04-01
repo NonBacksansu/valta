99098cb9a842ebea94e5496ae2ffb2500d4bac76
Moomin <79525093+NonBacksansu@users.noreply.github.com>
2026-04-01 21:29:26 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: "/YOUR-REPO-NAME/",   // 👈 ADD THIS LINE
+    base: "/valta/",   // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
